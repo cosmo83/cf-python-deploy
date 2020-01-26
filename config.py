7561a45cd745d06776098a885da535a44ebc2ecf
cosmo83 <arava@sat-infotech.com>
2020-01-26 12:41:20 +0530

    Initial Commit

---
+++ config.py
@@ -0,0 +1,10 @@
+from cfenv import AppEnv
+import os
+env = AppEnv()
+kavskdb = env.get_service(label='kavskagxdb')
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_ECHO = False
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATABASE_URI = "postgresql://username:password@localhost/database_name"
