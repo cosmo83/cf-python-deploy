6a749c08851de6adc9a55dd23903cd6380f245a7
vikaspandesatinfo <vikas.mkpande@gmail.com>
2020-01-26 14:01:49 +0530

    Changes

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 from cfenv import AppEnv
 import os
 env = AppEnv()
-kavskdb = env.get_service(label='kavskagxdb')
+kavskdb = env.get_service(label='aws-rds-postgresql').credentials
+
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "postgresql://username:password@localhost/database_name"
+SQLALCHEMY_DATABASE_URI = kavskdb.uri
+print("DB Information")
+print(SQLALCHEMY_DATABASE_URI)
